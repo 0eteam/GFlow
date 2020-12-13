4440df201b307c8ab388ee717faf6ac89133fd18
0eteam <1343831868@qq.com>
2020-12-13 03:18:22 -0800

    add_read

--- pynsq_read.py
+++ pynsq_read.py
@@ -1,9 +1,9 @@
+import pickle
 import time
 import nsq
 import threading
 import tornado
-
-
+start = time.time()
 
 class Get_flow(object):
     def __init__(self, num):
@@ -14,37 +14,48 @@ class Get_flow(object):
 
     def get_flow_func(self, number):
         def handler(message):
-            if len(self.data) < number:
-                self.read_lock.acquire()
-                message.enable_async()
-                self.data.append(str(message.body, encoding='utf-8'))
-                # print(str(message.body, encoding='utf-8'))
-                message.finish()
-                self.read_lock.release()
-            else:
-                self.ioloop.add_callback(self.ioloop.stop)
-            return True
+            try:
+                if len(self.data) < number:
+                    self.read_lock.acquire()
+                    message.enable_async()
+                    self.data.append(pickle.loads(message.body))
+                    # print(str(message.body, encoding='utf-8'))
+                    message.finish()
+                    self.read_lock.release()
+                else:
+                    self.ioloop.add_callback(self.ioloop.stop)
+                return True
+            except Exception as e:
+                # print(e)
+                pass
 
-        r = nsq.Reader(
-            message_handler=handler,
-            nsqd_tcp_addresses=['127.0.0.1:4150'],
-            topic='flow',
-            channel='read',
-            max_in_flight=1)
-        self.ioloop.start()
+        try:
+            r = nsq.Reader(
+                message_handler=handler,
+                nsqd_tcp_addresses=['192.168.75.129:4150'],
+                topic='flow',
+                channel='read',
+                max_in_flight=1)
+            self.ioloop.start()
+        except Exception as e:
+            # print(e)
+            pass
 
     def get_flow(self):
-        self.data = []
-        while True:
-            if len(self.data) < self.num:
-                t = threading.Thread(target=self.get_flow_func, args=(self.num,))
-                t.start()
-                t.join()
-            elif len(self.data) == self.num:
-                break
-            elif len(self.data) > self.num:
-                self.data = []
-        return self.data
+        try:
+            self.data = []
+            while True:
+                if len(self.data) < self.num:
+                    t = threading.Thread(target=self.get_flow_func, args=(self.num,))
+                    t.start()
+                    t.join()
+                elif len(self.data) >= self.num:
+                    self.data = self.data[:self.num]
+                    break
+            return self.data
+        except Exception as e:
+            # print(e)
+            pass
 
 if __name__ == "__main__":
     get_fl = Get_flow(10)
